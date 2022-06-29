ce76f48ec8243f9d48a9d9f575cd792ae24851cc
Makarenko Ivan <ivan.makarenko@free.fr>
2022-06-29 11:06:27 +0200

    Snackbar apparition

---
+++ snackbar/app.js
@@ -0,0 +1,8 @@
+function snack() {
+  const snackbar = document.getElementById("snackbar")
+
+  snackbar.classList.toggle('show')
+  setTimeout(function() {
+    snackbar.classList.toggle('show')
+  }, 3000)
+}
